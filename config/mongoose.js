afc2aad17482e870285efaf85c015fcffaf06f11
Abhijeet Kumar <abhijeetthakur7080@gmail.com>
2024-02-22 16:09:25 +0530

    mongoose updated

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // connecting to mongoose
-mongoose.connect('mongodb://127.0.0.1:27017/');
+mongoose.connect('mongodb+srv://abhijeetthakur7080:abhi@cluster0.25udaev.mongodb.net/');
 // mongoose database connection
 const db = mongoose.connection;
 db.on('error',console.error.bind(console,"Error connecting to MongoDb"));
